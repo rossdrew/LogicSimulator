4445840078d45c76f90d9bfc7f6e6dad182250e8
rossdrew <rosswilliamdrew@googlemail.com>
2016-05-05 12:27:19 +0100

    Optimised NAnd gate with less object creation

--- src/main/java/com/rox/logic/gate/compound/NAnd.java
+++ src/main/java/com/rox/logic/gate/compound/NAnd.java
@@ -2,7 +2,6 @@ package com.rox.logic.gate.compound;
 
 import com.rox.LogicGate;
 import com.rox.LogicValue;
-import com.rox.logic.gate.binary.*;
 
 /**
  * @Author rossdrew
@@ -13,17 +12,14 @@ import com.rox.logic.gate.binary.*;
 public class NAnd implements LogicGate{
     private LogicValue[] inputs;
 
-    private And andGate = new And();
-    private Not notGate = new Not();
-
     public void setInput(LogicValue... inputs) {
         this.inputs = inputs;
-
-        andGate.setInput(inputs);
-        notGate.setInput(andGate);
     }
 
     public boolean getValue() {
-        return notGate.getValue();
+        boolean inputA = inputs[0] == null ? false : inputs[0].getValue();
+        boolean inputB = inputs[1] == null ? false : inputs[1].getValue();
+
+        return !(inputA && inputB);
     }
 }
