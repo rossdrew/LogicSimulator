60b92e7f3fa92e2102056c677022275322b3b087
rossdrew <rosswilliamdrew@googlemail.com>
2016-05-05 13:07:52 +0100

    XNor gate

---
+++ src/main/java/com/rox/logic/gate/compound/XNOr.java
@@ -0,0 +1,23 @@
+package com.rox.logic.gate.compound;
+
+import com.rox.LogicGate;
+import com.rox.LogicValue;
+
+/**
+ * @Author rossdrew
+ * @Created 05/05/16.
+ */
+public class XNOr implements LogicGate {
+    private LogicValue[] inputs;
+
+    public void setInput(LogicValue... inputs) {
+        this.inputs = inputs;
+    }
+
+    public boolean getValue() {
+        boolean inputA = inputs[0] == null ? false : inputs[0].getValue();
+        boolean inputB = inputs[1] == null ? false : inputs[1].getValue();
+
+        return !(inputA^inputB);
+    }
+}
---
+++ src/test/java/com/rox/logic/gate/compound/TestXNor.java
@@ -0,0 +1,62 @@
+package com.rox.logic.gate.compound;
+
+import com.rox.LogicGate;
+import com.rox.logic.gate.binary.XOr;
+import com.rox.logic.gate.state.LogicalFalse;
+import com.rox.logic.gate.state.LogicalTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * @Author rossdrew
+ * @Created 05/05/16.
+ */
+public class TestXNor {
+    private LogicGate testGate;
+
+    @Before
+    public void setup(){
+        testGate = new XNOr();
+    }
+
+    @Test
+    public void testTwoTrueInputs(){
+        testGate.setInput(LogicalTrue.instance(), LogicalTrue.instance());
+        assertTrue(testGate.getValue());
+    }
+
+    @Test
+    public void testTwoFalseInputs(){
+        testGate.setInput(LogicalFalse.instance(), LogicalFalse.instance());
+        assertTrue(testGate.getValue());
+    }
+
+    @Test
+    public void testFalseBInput(){
+        testGate.setInput(LogicalTrue.instance(), LogicalFalse.instance());
+        assertFalse(testGate.getValue());
+    }
+
+    @Test
+    public void testFalseAInput(){
+        testGate.setInput(LogicalFalse.instance(), LogicalTrue.instance());
+        assertFalse(testGate.getValue());
+    }
+
+    @Test
+    public void testNullAInput(){
+        testGate.setInput(null, LogicalTrue.instance());
+        assertFalse(testGate.getValue());
+    }
+
+    @Test
+    public void testNullBInput(){
+        testGate.setInput(LogicalTrue.instance(), null);
+        assertFalse(testGate.getValue());
+    }
+
+    //Test for too many inputs?
+}
