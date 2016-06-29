b18aa26989d401830b8a118ec52ad13e5c0275fb
rossdrew <rosswilliamdrew@googlemail.com>
2016-06-29 10:10:09 +0100

    tmp

--- src/main/java/com/rox/TestFrame.java
+++ src/main/java/com/rox/TestFrame.java
@@ -7,36 +7,59 @@ import com.rox.ui.com.rox.ui.logic.gate.binary.NotCanvasImage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.security.NoSuchAlgorithmException;
 
 /**
  * @Author rossdrew
  * @Created 30/05/16.
  */
-public class TestFrame extends JFrame {
+public class TestFrame extends JFrame implements ComponentListener {
     public TestFrame(){
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setSize(200,200);
-//        setVisible(true);
-//        add(new JPanel(){
-//            private CanvasAdaptor adaptor;
-//
-//            @Override
-//            public void paint(Graphics g) {
-//                super.paint(g);
-//                if (adaptor == null)
-//                    adaptor = new GraphicsAdaptor(g);
-//
-//                CanvasImage canvasImage = new NotCanvasImage();
-//                canvasImage.setCanvasAdaptor(adaptor);
-//                canvasImage.setLocation(1,1,100,100);
-//
-//                canvasImage.draw();
-//            }
-//        });
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(200,200);
+        setVisible(true);
+
+        addComponentListener(this);
+
+        add(new JPanel(){
+            private CanvasAdaptor adaptor;
+
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                if (adaptor == null)
+                    adaptor = new GraphicsAdaptor(g);
+
+                CanvasImage canvasImage = new NotCanvasImage();
+                canvasImage.setCanvasAdaptor(adaptor);
+                canvasImage.setLocation(1,1,100,100);
+
+                canvasImage.draw();
+            }
+        });
     }
 
     public static void main(String args[]) throws NoSuchAlgorithmException {
         TestFrame frame = new TestFrame();
+    }
+
+    public void componentResized(ComponentEvent e) {
+        System.out.println("componentResized");
+        
+        repaint();
+    }
+
+    public void componentMoved(ComponentEvent e) {
+        System.out.println("componentMoved");
+    }
+
+    public void componentShown(ComponentEvent e) {
+        System.out.println("componentShown");
+    }
+
+    public void componentHidden(ComponentEvent e) {
+        System.out.println("componentHidden");
     }
 }
--- src/test/java/com/rox/ui/com/rox/ui/logic/gate/binary/TestNotCanvasImage.java
+++ src/test/java/com/rox/ui/com/rox/ui/logic/gate/binary/TestNotCanvasImage.java
@@ -6,21 +6,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
-
-import static junit.framework.TestCase.fail;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * @Author rossdrew
- * @Created 27/05/16.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class TestNotCanvasImage {
